2ec976e1e7477a9a82c10994f1b99bb0e5186cb2
RainerMauersberger23 <829532@mystma.org>
2021-01-07 17:10:09 +0000

    Top Five Movies Try 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def topFiveMovies(a, b, c, d, e):
-    return a, b, c, d, e
+    return a + " " + b + " " + c + " " + d + " " + e
 
 print(topFiveMovies("Star Wars", "Interstellar", "Martian", "Detective Pikachu", "Avengers"))
